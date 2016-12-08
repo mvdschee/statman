712a0f66334503f343a519ff60668f272a42d363
bartvanhoorn <bart_van_hoorn@hotmail.com>
2016-12-08 15:45:58 +0100

    bart dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,97 +1,80 @@
 @extends('layouts.app')
 
 @section('content')
-<body>
-
-<div class="container-fluid">
-  <h2>Monitool</h2>
-  <ul class="nav nav-pills" role="tablist">
-    <li class="active"><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-      <form class="navbar-form navbar-right">
-       <input type="text" class="form-control" placeholder="Search...">
-     </form>
-        
-    <li class="dropdown">
-      <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-      Tutorials <span class="caret"></span></a>
-      <ul class="dropdown-menu" role="menu">
-        <li><a href="#">HTML</a></li>
-        <li><a href="#">CSS</a></li>
-        <li><a href="#">JavaScript</a></li>                        
-      </ul>
-    </li>
-  </ul>
+<div class="panel-group" id="accordion">
+  <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+  </a>
 </div>
-  
-</body>
-
-<nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Monitool</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Help</a></li>
-          </ul>
-          <form class="navbar-form navbar-right">
-            <input type="text" class="form-control" placeholder="Search...">
-          </form>
-        </div>
-      </div>
-    </nav>
-
-
-<div style="padding-bottom:250px;"></div>
-<div class="container">
-  <div class="row mediachannels text-center">
-    <div class="col-md-4">
-      <div style="border: 1px solid black";>
-        <h3>Facebook</h3>
-        <br>hier meer facebook data<br>
-          <div style="width: 200px;
-                height: 80px;
-                margin-left: 80px;
-                margin-top: 20px;
-                background-color: #eee;
-                overflow: auto;">Hier heeeeeeeeeeel veeel tekst dat onzinning is voor de overlfow wat je weet zelf G vat het niet personal man
-          </div>
-        </div>
-      </div>    
-    <div class="col-md-4">
-    <h3>Youtube</h3>
-    <br>hier meer Youtube data<br>
-     <div style="width: 200px;
-                height: 80px;
-                margin-left: 80px;
-                margin-top: 20px;
-                background-color: #eee;
-                overflow: auto;">
-                Hier heeeeeeeeeeel veeel tekst dat onzinning is voor de overlfow wat je weet zelf G vat het niet 
-                personal man
-      </div>
+<div id="collapse1" class="panel-collapsed collapse out">
+  <div class="panel-body">Hier zijn de filter opties</div>
+    <div class="checkbox">
+      <label><input type="checkbox"> Filter options 1</label>
     </div>
-    <div class="col-md-4">
-    <h3>Twitter</h3>
-    <br>hier meer Facebook data<br>
-      <div style="width: 200px;
-                height: 80px;
-                margin-left: 80px;
-                margin-top: 20px;
-                background-color: #eee;
-                overflow: auto;">Hier heeeeeeeeeeel veeel tekst dat onzinning is voor de overlfow wat je weet zelf G vat het niet personal man
+      <div class="checkbox">
+        <label><input type="checkbox"> Filter options 2</label>
+      </div>
+        <div class="dropdown">
+          <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown
+            <div class="btn btn-primary accordion.active">
+            <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+              <li><a href="#">Ein</a></li>
+              <li><a href="#">Zwei</a></li>
+              <li><a href="#">Drei</a></li>
+            </ul>
+        </div> 
       </div>
     </div>
   </div>
 </div>
+<div class="container-fluid">
+ <div class="row">
+  <div class= "col-md-8 graph-gyazo">
+   <img src="./public/img/graph-gyazo.png" class="img-rounded" alt="Cinque Terre" width="1250" height="384">
+  </div>
+    <div class="col-md-2 navbar-right" style="background-color:white; margin-top: -25px; height:375px;">
+      <ul class="nav">
+        <img src="./public/img/ic_insert_chart_black_24dp_2x.png"> 
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+        <img src="./public/img/ic_filter_list_black_24dp_2x.png"></a>        
+        <li class="active"><a href="#">Overview<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Reports</a></li>
+        <li><a href="#">Analytics</a></li>
+        <li><a href="#">Export</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+<div class="container-fluid">
+  <div class="rowmediachannels text-center">
+    <div class="rowmediaeach col-md-4">
+    <h3>Overall</h3>
+    <br>hier meer facebook data<br>
+    <img src="./public/img/procentdonut.png"></a>        
 
+  </div>
+
+  <div class="rowmediaeach col-md-4">
+  <h3>Facebook</h3>
+  <br>hier meer Facebook data<br>
+  </div>
+  
+  <div class="rowmediaeach col-md-4">
+  <h3>Youtube</h3>
+  <br>hier meer Youtube data<br>
+  <div style="width: 400px;
+      height: 150px;
+      margin-left: 40px;
+      margin-top: 20px;
+      background-color: #eee;
+      overflow: auto;">Lorum ipsum Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.
+      Lorum ipsum Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 @endsection
