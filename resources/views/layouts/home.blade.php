0549a300e166d145fb25ca87c80ed421466339ba
Max van der Schee <m.v.d.schee@ewake.nl>
2017-11-07 17:03:24 +0100

    changed metadata

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -6,54 +6,38 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="robots" content="index, follow">
-
-		{{-- for social media --}}
 		<meta property="og:type" content="website">
 		<meta property="og:url" content="https://statman.nl/">
 		<meta property="og:locale" content="nl_NL">
 		<meta property="og:site_name" content="Statman">
 		<meta property="og:title" content="Statman">
-		<meta property="og:description" content="Statman is a tool to track the behavior of your targeted audience and engagement throughout a selection of platforms.">
+		<meta property="og:description" content="Tracking and managing your story can be challenging, in order to help you engage with your story on a deeper level we developed a tool. The tool allows you to track your story on multiple platforms.">
 		<meta property="og:image" content="https://statman.nl/image.png">
-
-		{{-- search engine descriptions --}}
-		<meta name="description" content="Statman is a tool to track the behavior of your targeted audience and engagement throughout a selection of platforms.">
+		<meta name="description" content="Tracking and managing your story can be challenging, in order to help you engage with your story on a deeper level we developed a tool. The tool allows you to track your story on multiple platforms.">
 		<meta name="keywords" content="statman,analytics,tracking,social media">
-		<meta name="author" content="DataBoyz">
-
-		{{-- itemprops --}}
+		<meta name="author" content="YumYum">
 		<meta itemprop="name" content="Statman">
 		<meta itemprop="headline" content="Statman">
-		<meta itemprop="description" content="Statman is a tool to track the behavior of your targeted audience and engagement throughout a selection of platforms.">
+		<meta itemprop="description" content="Tracking and managing your story can be challenging, in order to help you engage with your story on a deeper level we developed a tool. The tool allows you to track your story on multiple platforms.">
 		<meta itemprop="image" content="https://statman.nl/image.png">
 
-		{{-- mobile browser data --}}
-		<meta name="apple-mobile-web-app-title" content="Statman">
-		<meta name="theme-color" content="#262d66">
-		<link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png">
+		<title>Statman | Home</title>
 
-		<meta name="application-name" content="Statman">
-		<meta name="msapplication-navbutton-color" content="#000000">
-
-		{{-- favicons --}}
-		<link rel="manifest" href="/public/manifest.json">
-		<link rel="mask-icon" href="/public/safari-pinned-tab.svg" color="#262d66">
-		<link rel="shortcut icon" href="{{ URL::asset("/resources/assets/img/favicon.ico") }}">
-
-		<title>Statman</title>
 		<link rel="stylesheet" href="{{ URL::asset("/resources/assets/css/home.css") }}">
+
 		<script type="text/javascript">
-		  var _paq = _paq || [];
-		  _paq.push(['trackPageView']);
-		  _paq.push(['enableLinkTracking']);
-		  (function() {
-		    var u="https://analytics.ewake.nl/";
-		    _paq.push(['setTrackerUrl', u+'piwik.php']);
-		    _paq.push(['setSiteId', '6']);
-		    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-		    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-		  })();
+			var _paq = _paq || [];
+			_paq.push(['trackPageView']);
+			_paq.push(['enableLinkTracking']);
+			(function() {
+				var u="https://analytics.ewake.nl/";
+				_paq.push(['setTrackerUrl', u+'piwik.php']);
+				_paq.push(['setSiteId', '6']);
+				var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+				g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+			})();
 		</script>
+		<noscript><p><img src="https://analytics.ewake.nl/piwik.php?idsite=6&rec=1" style="border:0;" alt="" /></p></noscript>
 	</head>
 
 	<body class="body">
