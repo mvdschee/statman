30fd4f9084c4d8ef9363d176a9a1875e93701890
Max van der Schee <m.v.d.schee@ewake.nl>
2017-11-07 23:55:36 +0100

    added all sections

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,10 @@
         <p>We know from experience that it is important to have a clear overview of the impact your story has with your target audience.</p>
         <a class="button" href="#">Let's see how</a>
       </article>
-      <figure class="image right"> <img src="{{ URL::asset("/resources/assets/img/workspace.svg") }}" alt="workspace"></figure>
+      <figure class="image right"><img src="{{ URL::asset("/resources/assets/img/workspace.svg") }}" alt="workspace"></figure>
     </section>
 
-    <section class="features">
+    <section class="features clear">
       <ul class="features-group">
         <li class="features-item">facebook</li>
         <li class="features-item">facebook</li>
@@ -21,5 +21,47 @@
         <p>Tracking and managing your story can be challenging, in order to help you engage with your story on a deeper level we developed a tool. The tool allows you to track your story on.</p>
       </article>
     </section>
+
+    <section class="storyworld clear">
+      <figure class="storyworld-image left">
+        <img src="{{ URL::asset("/resources/assets/img/workspace.svg") }}" alt="workspace">
+      </figure>
+      <article class="storyworld-text right">
+        <h1>Social media platforms</h1>
+        <p>Tracking and managing your story can be challenging, in order to help you engage with your story on a deeper level we developed a tool. The tool allows you to track your story on.</p>
+      </article>
+    </section>
+
+    <section class="security clear">
+      <ul class="security-group">
+        <li class="security-item">
+          <img src="{{ URL::asset("/resources/assets/img/workspace.svg") }}" alt="workspace">
+          <h2>security</h2>
+          <p>I close the curtains and fall back on my bed, covering my face with my pillow, but the smell becomes stronger and stronger. “Damn Skunk!”</p>
+        </li>
+        <li class="security-item">
+          <img src="{{ URL::asset("/resources/assets/img/workspace.svg") }}" alt="workspace">
+          <h2>security</h2>
+          <p>I close the curtains and fall back on my bed, covering my face with my pillow, but the smell becomes stronger and stronger. “Damn Skunk!”</p>
+        </li>
+        <li class="security-item">
+          <img src="{{ URL::asset("/resources/assets/img/workspace.svg") }}" alt="workspace">
+          <h2>security</h2>
+          <p>I close the curtains and fall back on my bed, covering my face with my pillow, but the smell becomes stronger and stronger. “Damn Skunk!”</p>
+        </li>
+      </ul>
+    </section>
+
+    <section class="endorsment clear">
+      <figure class="endorsment-image left">
+        <img src="{{ URL::asset("/resources/assets/img/workspace.svg") }}" alt="workspace">
+      </figure>
+      <article class="endorsment-text right">
+        <h1>Social media platforms</h1>
+        <p>Tracking and managing your story can be challenging, in order to help you engage with your story on a deeper level we developed a tool. The tool allows you to track your story on.</p>
+      </article>
+    </section>
+
+
 
 @endsection
