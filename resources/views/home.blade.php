8ef1cadd24c7bc281614bf04589156deb826f1a7
Max van der Schee <m.v.d.schee@ewake.nl>
2017-11-10 17:49:34 +0100

    homepage is fully responsive :sparkles:

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.home')
 @section('content')
+    @if ($message)
+      <div class="message">
+        {{ $message }}
+      </div>
+    @endif
     <section class="hero">
+      <figure class="image right"><img src="{{ URL::asset("/resources/assets/img/workspace.svg") }}" alt="workspace"></figure>
       <article class="text left">
         <h2 itemprop="headline">The way you see your story, that is what we care about.</h2>
         <p>We know from experience that it is important to have a clear overview of the impact your story has with your target audience.</p>
@@ -12,7 +18,6 @@
           @endif
         </div>
       </article>
-      <figure class="image right"><img src="{{ URL::asset("/resources/assets/img/workspace.svg") }}" alt="workspace"></figure>
     </section>
 
     <section class="social clear">
