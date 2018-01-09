5f343d428598234bb1c34c10e15d7bba4cba7f2d
Michiel Meilink <michielmeilink@gmail.com>
2018-01-09 12:10:46 +0100

    Styled the beginning of post statistics detail 🎨

--- resources/views/dashboards/post_detail.blade.php
+++ resources/views/dashboards/post_detail.blade.php
@@ -2,4 +2,28 @@
 
 @section('content')
 
+<div class="container">
+  <section class="window posts-statics">
+      <div class="middle">
+        <div class="middle-block-detail">
+          <div class="circle">
+            <img src="{{ URL::asset("../assets/img/fb.svg") }}">
+            <div class="titles">
+              <h3>Aflevering reminder</h3>
+              <p class="light">Coldfeet</p>
+            </div>
+          </div>
+          <div class="circle-text">
+            <p>Out believe has request not how comfort evident. Up delight cousins we feeling minutes. Genius has looked end piqued spring. Down has rose feel find man. Learning day desirous informed expenses material returned six the. She enabled invited exposed him another. Reasonably conviction solicitude me mr at discretion reasonable. </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="bottom">
+        <span class="">
+
+        </span>
+      </div>
+  </section>
+</div>
 @endsection
