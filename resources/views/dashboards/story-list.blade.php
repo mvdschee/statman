7b3e66b5ac2ac26a15d605ca24e7e8e4f58aa64c
Hans Pol <hans@mbp-van-hans-2.access.nhl.nl>
2018-01-15 12:43:39 +0100

    New story overview

--- resources/views/dashboards/story-list.blade.php
+++ resources/views/dashboards/story-list.blade.php
@@ -1,46 +1,37 @@
 @extends('layouts.master')
 @section('content')
 
-<div class="panel">
-	<legend><h2>Stories</h2></legend>
+<link rel="stylesheet" href="{{ URL::asset("assets/css/dashboards/second.nested.css") }}">
+<div class="panel" id="page">
+	<div class="legend-block">
+		<legend><h1 style="cursor: pointer" id="button">Story Overview</h1></legend>
+		<p>Select the story you want to work with.</p>
+	</div>
     @if (session('check'))
     <div class="error">
     	{{ session('check') }}
     </div>
     @endif
 	@if ($data)
-		<table class="storylist">
-			<tr class="tableheader">
-				<th>Project</th>
-				<th>Role</th>
-				<th class="options-td">Invite</th>
-			</tr>
+	<div class="container-story">
+		<div class="project-group">
 			@foreach ($data as $data)
-				<tr>
-					<td>
-						<form method="POST" action="{{ url('/story-list/favorite') }}">
-							{{ csrf_field() }}
-							<input type="hidden" name="project" value="{{ $data['project_id'] }}">
-							@if ($user->favorite == $data['project_id'])
-								<button class="unfavorite icon-star"></button>
-							@else
-								<button class="favorite icon-star-o"></button>
-							@endif
-						</form>
-						<a href="{{ url('/dashboard/') }}/{{ $data['project_id'] }}" class="project_name">{{ $data['project_name'] }}</a>
-					</td>
-					<td class="role-name">{{ $data['role'] }}</td>
-					<td class="options-td">
-						<form method="POST" action="{{ url('/story-list') }}">
-							{{ csrf_field() }}
-							<input type="hidden" name="option" value="{{ $data['project_id'] }}">
-							<a href="/storysettings/{{ $data['project_id'] }}">settings</a>
-							<button type="submit"><i class='icon-user-add'></i></button>
-						</form>
-					</td>
-				</tr>
+					<div class="project-item">
+						<img src="https://images.pexels.com/photos/785544/pexels-photo-785544.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb" alt="">
+						<div class="info-block">
+							<div class="information">
+								<h4>{{ $data['project_name'] }}</h4>
+								<p>Date</p>
+							</div>
+							<a href="/storysettings/{{ $data['project_id'] }}"><i class="cog"></i></a>
+						</div>
+					</div>
 			@endforeach
-		</table>
+			<a class="add-project-item" href='{{ url('/create-story') }}'>
+				<div><i></i></div>
+			</a>
+		</div>
+	</div>
 	@else
 		<h3 class="no-story">You have no stories yet, do you want to create a new one?</h3>
 		<a class="button" href='{{ url('/create-story') }}'>Create a story</a>
@@ -56,14 +47,26 @@
 			</tr>
 			@foreach ($invites as $invite)
 				<tr>
-					<td class="add-button">
-						<a href="/invited/{{ $invite['token'] }}"><i class='icon-plus'></i></a>
-					</td>
+					<td class="add-button"><a href="/invited/{{ $invite['token'] }}"><i class='icon-plus'></i></a></td>
 					<td>{{ $invite['project_name'] }}</td>
 				</tr>
 			@endforeach
 		</table>
 	@endif
 </div>
+<script type="text/javascript">
+window.onload = function() {
+	var button = document.getElementById('button');
+	button.onclick = function(){
+		var body = document.getElementById('page')
+		if(body.classList == ""){
+			body.classList = "spinnerino";
+		} else {
+			body.classList = "";
+		}
+	}
+
+};
+</script>
 
 @endsection
